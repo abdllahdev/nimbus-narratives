cd7cce0c37d8bdb807ec920a9ae009e2846cd17b
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2023-06-04 21:27:09 +0300

    feat: home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://algorizr.com",
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {},
-  daisyui: {
-    themes: ["night"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue["400"],
+        secondary: colors.indigo["400"],
+        accent: colors.pink["400"],
+        neutral: colors.neutral["900"],
+        "neutral-content": colors.neutral["100"],
+        base: colors.slate["900"],
+        "base-content": colors.slate["100"],
+        info: colors.blue["500"],
+        success: colors.green["400"],
+        warning: colors.yellow["400"],
+        error: colors.red["400"],
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
