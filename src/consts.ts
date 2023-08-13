67bedc2f9ae584296d0e61d8b9ad6a9d4461f540
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2023-08-13 20:17:32 +0100

    refactor: move social media handles to consts.ts and change them

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,7 @@
 export const SITE_NAME = "NimbusNarratives";
-export const SITE_TITLE = "NimbusNarratives | A personal blog by Abdullah Elsayed (@algorizr)";
-export const SITE_DESCRIPTION = "NimbusNarratives | A personal blog by Abdullah Elsayed (@algorizr)";
+export const SITE_TITLE = "NimbusNarratives | A personal blog by Abdullah Elsayed (@abdllahdev)";
+export const SITE_DESCRIPTION =
+  "NimbusNarratives | A personal blog by Abdullah Elsayed (@abdllahdev)";
+export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
+export const GITHUB_HANDLE = "https://github.com/abdllahdev";
+export const CLOUDINARY_NAME = "example";
